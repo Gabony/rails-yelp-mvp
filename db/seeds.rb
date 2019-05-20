df50d106ab107f3db18e509edf79de5c628cf1b9
Thomas CHOPITEL <thomas.chopitel@yahoo.fr>
2019-05-20 12:45:04 +0200

    update routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      'Londres',
+    category:     'italian'
+  },
+  {
+    name:         'Le Côtier',
+    address:      'Paris 12',
+    category:     'french'
+  },
+  {
+    name:         'Le Dragon',
+    address:      'PAris 9',
+    category:     'chinese'
+  },
+  {
+    name:         'Chez Antonio',
+    address:      'Paris 16',
+    category:     'italian'
+  },
+  {
+    name:         'Des frites',
+    address:      'Paris 1',
+    category:     'belgian'
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
