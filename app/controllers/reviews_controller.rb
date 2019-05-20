17b19040e2279ddc007f9845fcd5a4fd12a5ce29
Thomas CHOPITEL <thomas.chopitel@yahoo.fr>
2019-05-20 15:12:26 +0200

    update exo 2

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,14 @@
 class ReviewsController < ApplicationController
 
+  def index
+    @reviews = Review.where(restaurant_id: params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
-
 
   def create
     @review = Review.new(review_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :restaurants
   resources :restaurants do
     resources :reviews, only: [:index, :new, :create]
   end
